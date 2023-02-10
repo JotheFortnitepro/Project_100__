3852f7a468d82877b4f8e57af798b1588144a82c
JotheFortnitepro <115483897+JotheFortnitepro@users.noreply.github.com>
2023-02-10 12:23:28 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,23 @@
+// Your web app's Firebase configuration
+//ADD YOUR FIREBASE LINKS HERE
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    /*
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+    */
+}
+
+
+
